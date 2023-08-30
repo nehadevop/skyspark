d205f31acd8737f7eb5dd00398ed1379dd82d545
nehadevop <134670704+nehadevop@users.noreply.github.com>
2023-08-30 16:08:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// JavaScript source code
+
+const search = document.getElementById("search");
+
+search.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        console.log(search.value)
+        fetch(`http://api.weatherapi.com/v1/current.json?key=df286c56ea5d4161b7775511233008&q=${search.value}&aqi=no`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data.location)
+                const country = document.getElementById("country")
+                const city  = document.getElementById("city")
+                const temperature = document.getElementById("temperature")
+                country.innerHTML = data.location.country
+                city.innerHTML = data.location.name
+                temperature.innerHTML = data.current.temp_c
+                const card = document.getElementById('card').style = "display:block;"
+                search.innerHTML=""
+
+
+            })
+        
+
+    }
+});
+search.innerText = " "
+
+
+    
